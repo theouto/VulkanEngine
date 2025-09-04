333d1bab465b8042074e425efc0e70ddec36285b
theouto <tomas@otero.pro>
2025-09-04 16:59:54 +0200

    cleanup

---
+++ VulkanLearning_real1/build/src/lve_object_master.cpp.d
@@ -0,0 +1,13 @@
+build/./src/lve_object_master.cpp.o: src/lve_object_master.cpp \
+ src/../include/lve_object_master.hpp src/../include/lve_game_object.hpp \
+ src/../include/lve_model.hpp src/../include/lve_device.hpp \
+ src/../include/lve_window.hpp src/../include/lve_buffer.hpp \
+ src/../include/lve_textures.hpp src/../include/lve_descriptors.hpp
+src/../include/lve_object_master.hpp:
+src/../include/lve_game_object.hpp:
+src/../include/lve_model.hpp:
+src/../include/lve_device.hpp:
+src/../include/lve_window.hpp:
+src/../include/lve_buffer.hpp:
+src/../include/lve_textures.hpp:
+src/../include/lve_descriptors.hpp:
