5720f1d7dc1bc58780d9ce1c36a952d8b481ea54
theouto <tomas@otero.pro>
2025-09-06 14:16:54 +0200

    nevermind, I'm bringing it back as its own thing

---
+++ VulkanLearning_real1/include/lve_object_master.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "lve_game_object.hpp"
+#include "lve_textures.hpp"
+
+
+//The point of the game object master will be to streamline the process of having game objects,
+//also easing the process of having objects with different textures in the scene while managing their material properties
+//as not all assets will have the material assets needed to fully utilise the programmed shaders
+//
+//This is written here as it is now because I want to pressure myself to finish this at some point, and this looks silly
+
+namespace lve
+{
+	struct lvobj
+	{
+		//TODO
+	}
+	
+	public:
+
+	private:
+
+}
