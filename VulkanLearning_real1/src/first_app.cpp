cc1fc1f1f9371e12bdffd6caf85f0e4a84da529b
theouto <tomas@otero.pro>
2026-02-27 19:24:42 +0100

    scene saving and some cleanup

--- VulkanLearning_real1/src/first_app.cpp
+++ VulkanLearning_real1/src/first_app.cpp
@@ -85,29 +85,6 @@ namespace lve
 	
         
         sceneManager.load("scenes/test_scene.ths", *lveRenderer.globalPool);
-        /*
-        std::vector<glm::vec3> lightColors{
-            {1.f, .1f, .1f},
-            {.1f, .1f, 1.f},
-            {.1f, 1.f, .1f},
-            {1.f, 1.f, .1f},
-            {.1f, 1.f, 1.f},
-            {1.f, 1.f, 1.f},
-            {.0157f, 0.824f, 0.745f}    //0x04d3be
-        };
-
-        for (int i = 0; i < lightColors.size(); i++) 
-        {
-            auto pointLight = LveGameObject::makePointLight(0.7f);
-            pointLight.color = lightColors[i];
-            auto rotateLight = glm::rotate(
-                glm::mat4(1.f),
-                (i * glm::two_pi<float>()) / lightColors.size(),
-                { 0.f, -1.f, 0.f });
-            pointLight.transform.translation = glm::vec3(rotateLight * glm::vec4(-1.f, -1.f, -1.f, 1.f));
-            gameObjects.emplace(pointLight.getId(), std::move(pointLight));
-        }
-        */
 
 	    // https://www.glfw.org/docs/3.3/input_guide.html#raw_mouse_motion <- important
         glfwSetInputMode(lveWindow.getGLFWwindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
