2c7847e6bc7925002f44a808d0db15a538b45211
theouto <tomas@otero.pro>
2026-04-08 20:21:11 +0200

    improved shader compilation script

--- VulkanLearning_real1/shaders/shadercompile.sh
+++ VulkanLearning_real1/shaders/shadercompile.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-glslc main_pass/skybox.frag -o compiled/skybox.frag.spv
-glslc main_pass/skybox.vert -o compiled/skybox.vert.spv
-glslc main_pass/point_light.frag -o compiled/point_light.frag.spv
-glslc main_pass/point_light.vert -o compiled/point_light.vert.spv
-glslc main_pass/simple_shader.frag -o compiled/simple_shader.frag.spv
-glslc main_pass/simple_shader.vert -o compiled/simple_shader.vert.spv
-glslc lighting/shadowmap.frag -o compiled/shadowmap.frag.spv
-glslc lighting/shadowmap.vert -o compiled/shadowmap.vert.spv
-glslc buffers/normal_spec.vert -o compiled/normal_spec.vert.spv
-glslc buffers/normal_spec.frag -o compiled/normal_spec.frag.spv
-glslc buffers/depth.vert -o compiled/depth.vert.spv
-glslc buffers/depth.frag -o compiled/depth.frag.spv
-glslc post_effects/AO.vert -o compiled/AO.vert.spv
-glslc post_effects/AO.frag -o compiled/AO.frag.spv
-glslc testing/bindless_test.vert -o compiled/bindless_test.vert.spv
-glslc testing/bindless_test.frag -o compiled/bindless_test.frag.spv
+shaderfiles=()
+while IFS= read -r line; do
+  if [[ "${line:0:8}" != "compiled" ]]; then
+    shaderfiles+=("$line")
+  fi
+done < <(rg --files)
+
+IFS='/'
+count=0
+for val in "${shaderfiles[@]}"; do
+
+  [[ $val == "shadercompile.sh" ]] && continue
+
+  read -ra isolated <<<"$val"
+  comp="compiled/"${isolated[1]}".spv"
+
+  if [ ! -f "$comp" ] || [[ $(date -r "$val" +%s) > $(date -r "$comp" +%s) ]]; then
+    glslc "$val" -o "$comp"
+    echo "compiled "$val""
+  fi
+
+done
